53d042d2370fac3ac769699d44281a72750ef99f
ernane <ernanecarvalho89@gmail.com>
2021-12-06 21:05:43 -0300

    projeto

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BtnWhatsapp from './BtnWhatsapp'
+
+const Footer = () => {
+    return (
+        <div className='p-4 sm:flex bg-green-100 sm:justify-between items-center sm:justify-left '>
+            <div>
+                <h5>Fran Di Carvalho</h5>
+                <p>Monte Carmelo-MG</p>
+                <p>Instagran: @frandicarvalho</p>
+            </div >            
+        </div>
+    )
+}
+
+export default Footer
